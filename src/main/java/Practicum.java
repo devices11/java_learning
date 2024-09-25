15f4dc6cc63d4ad5ffbdaf4f73af16c8c75852c1
Izotov Valentin <vizotov@vtb.ru>
2024-09-25 12:00:13 +0300

    Practicum test

--- src/main/java/Practicum.java
+++ src/main/java/Practicum.java
@@ -9,6 +9,9 @@ import java.util.function.Predicate;
 //}
 
 public class Practicum {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
     public static void main(String[] args) {
         // сохраните лямбду в переменную
